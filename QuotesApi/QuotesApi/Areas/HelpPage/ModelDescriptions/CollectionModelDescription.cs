719e136a14947b4ae638783cf296d021dbfbd446
asfend <asfend420@gmail.com>
2019-07-29 23:46:58 +0500

    Quote Deleted

--- QuotesApi/QuotesApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace QuotesApi.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
