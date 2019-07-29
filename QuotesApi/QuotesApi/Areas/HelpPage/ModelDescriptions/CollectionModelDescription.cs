ffaf71bd0b33970a675e54b81aa59bb1f8045cda
asfend <asfend420@gmail.com>
2019-07-29 22:05:35 +0500

    Quotes Added

---
+++ QuotesApi/QuotesApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace QuotesApi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
