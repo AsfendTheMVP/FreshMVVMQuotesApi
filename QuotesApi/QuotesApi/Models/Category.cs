436b66dee63ef7ae2421d7889f1ee08097fc340e
asfend <asfend420@gmail.com>
2019-07-29 23:47:56 +0500

    Quote Added

---
+++ QuotesApi/QuotesApi/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QuotesApi.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ColorCode { get; set; }
+    }
+}
